dd2210f16a709095187a0f2c8dfb3a7ac84458dd
mswift42 <martin.haesler@gmail.com>
2019-10-10 05:00:21 +0100

    Add darken / lighten methods.

--- gtc.go
+++ gtc.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"github.com/lucasb-eyer/go-colorful"
+)
+
 func main() {
 
+}
+
+func hasDarkBG(col *colorful.Color) bool {
+	l, _, _ := col.Lab()
+	return l < 0.5
+}
+
+func lighten(col *colorful.Color, factor float64) string {
+	white, _ := colorful.Hex("#ffffff")
+	return col.BlendLab(white, factor).Hex()
+}
+
+func darken(col *colorful.Color, factor float64) string {
+	black, _ := colorful.Hex("#000000")
+	return col.BlendLab(black, factor).Hex()
+}
+// ThemeMap represents all colors of an ui theme.
+// It holds the theme's background color and darker / lighter
+// shades thereof, foregroundcolors in varying shades and colors,
+// and a boolean that is set to true if the background color counts
+// as "dark".
+type ThemeMap struct {
+	DarkBG      bool
+	Fg1         string
+	Fg2         string
+	Bg1         string
+	Bg01        string
+	Bg2         string
+	Bg3         string
+	Bg4         string
+	Builtin     string
+	Keyword     string
+	Constant    string
+	Comment     string
+	Func        string
+	String      string
+	Type        string
+	Warning     string
+	Warning2    string
+	InvBuiltin  string
+	InvKeyword  string
+	InvType     string
+	InvFunc     string
+	InvString   string
+	InvWarning  string
+	InvWarning2 string
 }

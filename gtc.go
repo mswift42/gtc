405236b527aec891886962419cb25c10c5459f4c
mswift42 <martin.haesler@gmail.com>
2019-10-29 16:44:57 +0000

    run initcli on start.

--- gtc.go
+++ gtc.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/mswift42/gtc/cl"
+	"os"
+)
 
+func main() {
+	app := cl.InitCli()
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
 }
